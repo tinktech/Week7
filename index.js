491ab94c987b8af9d81a4fd5124dd4a0881d5eb9
tdayetech <150866869+tdayetech@users.noreply.github.com>
2024-01-11 20:24:36 -0600

    four questions finished

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /*
+All questions should be printed to your Browser's console using console.log()
+
 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
     a. Programmatically subtract the value of the first element in the array from the value in the last element of the array.
         - Do not use numbers to reference the last element, find it programmatically.
@@ -42,3 +44,44 @@
 13. Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
 */
 
+// 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+console.log(`Ages array: [${ages}]`);
+// a. Subtract first from last
+console.log(`First value subtracted from last: ${ages[(ages.length - 1)] - ages[0]}`);
+// b. Add new age
+ages.push(38);
+console.log(`Ages array: [${ages}]`);
+console.log(`First value subtracted from last: ${ages[(ages.length - 1)] - ages[0]}`);
+// c. Loop, calculate average age
+let aSum = 0;
+for (let i = 0; i < ages.length; i++) {
+    aSum += ages[i];
+}
+let aAverage = (aSum / ages.length);
+console.log(`Average age: ${aAverage}`);
+
+// 2. Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
+console.log(`Names array: [${names}]`);
+// a. Loop, calculate average number of letters per name
+let nSum = 0;
+for (let i = 0; i < names.length; i++) {
+    nSum += names[i].length;
+}
+let nAverage = (nSum / names.length);
+console.log(`Average number of letters per name: ${nAverage}`);
+// b. Loop, concatenate all names together with spaces
+let namesCon = '';
+for (let i = 0; i < names.length; i++) {
+    namesCon += (`${names[i]} `);
+}
+console.log(`Names concatenated with spaces: ${namesCon}`);
+
+// 3. How do you access the last element of any array?
+console.log(`Last element of an array can be accessed using: (array.length - 1)`);
+
+// 4. How do you access the first element of any array?
+console.log(`First element of an array can be accessed using: array[0]`);
+
+// 5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
